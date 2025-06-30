c2ee3f8dbfe1f86053a660ecd7bdda8c4a4b3008
Alice Smith <alice@webage.com>
2025-06-30 09:57:36 -0400

    updated server express to work w react app and updated gitignore

--- server/server.js
+++ server/server.js
@@ -4,8 +4,17 @@
 // console.log('Another change in the dev branch.');
 import express from 'express';
 import { promises as fs } from 'fs';
+import { MongoClient, ObjectId } from 'mongodb';
+import dotenv from 'dotenv';
+import cors from 'cors';
+dotenv.config();
+const url = process.env.MONGO_DB_URL;
+const dbName = process.env.MONGO_DB;
+const collectionName = process.env.MONGO_DB_COLLECTION;
+
 const app = express();
 const PORT = 3000;
+app.use(cors());
 // // Endpoint to read and send JSON file content
 // app.get('/socks', async (_req, res) => {
 //     try {
@@ -17,23 +26,29 @@ const PORT = 3000;
 //         res.status(500).send("Hmmm, something smells... No socks for you! ☹");
 //     }
 // });
-// app.get('/socks', async (req, res) => {
-//     try {
-//         // Console log the entire request object
-//         console.log(req);
-//         // Console log specific parts of the request
-//         console.log("Headers:", req.headers);
-//         console.log("URL:", req.url);
-//         console.log("Method:", req.method);
-//         console.log("Query parameters:", req.query);
-//         const data = await fs.readFile('../data/socks.json', 'utf8');
-//         const jsonObj = JSON.parse(data);
-//         res.json(jsonObj);
-//     } catch (err) {
-//         console.error("Error:", err);
-//         res.status(500).send("Hmmm, something smells... No socks for you! ☹");
-//     }
-// });
+app.get('/socks', async (req, res) => {
+    try {
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        const socks = await collection.find({}).toArray();
+        res.json(socks);
+        // Console log the entire request object
+        // console.log(req);
+        // // Console log specific parts of the request
+        // console.log("Headers:", req.headers);
+        // console.log("URL:", req.url);
+        // console.log("Method:", req.method);
+        // console.log("Query parameters:", req.query);
+        // const data = await fs.readFile('../data/socks.json', 'utf8');
+        // const jsonObj = JSON.parse(data);
+        res.json(jsonObj);
+    } catch (err) {
+        console.error("Error:", err);
+        res.status(500).send("Hmmm, something smells... No socks for you! ☹");
+    }
+});
+
 app.get('/socks/:color', async (req, res) => {
     try {
         const requestedColor = req.params.color.toLowerCase();
@@ -42,7 +57,7 @@ app.get('/socks/:color', async (req, res) => {
         const jsonObj = JSON.parse(data);
 
         // Filter socks by color
-        const matchingSocks = jsonObj.filter(sock => 
+        const matchingSocks = jsonObj.filter(sock =>
             sock.color && sock.color.toLowerCase() === requestedColor
         );
 
@@ -58,7 +73,26 @@ app.get('/socks/:color', async (req, res) => {
 });
 // Middleware to parse JSON bodies
 app.use(express.json());
-app.post('/socks', async (req, res) => {
+app.post('/socks/search', async (req, res) => {
+    try {
+        // TODO: Add code that can search MongoDB based on a color value
+        // from the Search text box.
+        const { searchTerm } = req.body;
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        const regex = new RegExp(searchTerm, "i"); // Create a case-insensitive regular expression
+        const socks = await collection
+            .find({ "sockDetails.color": regex })
+            .toArray();
+        res.json(socks);
+        // console.log(socks)
+    } catch (err) {
+        console.error('Error:', err);
+        res.status(500).send('Hmm, something doesn\'t smell right... Error searching for socks');
+    }
+});
+app.post('/user', async (req, res) => {
     try {
         // Obligatory reference to POST Malone
         console.log("If POST Malone were a sock, he'd be the one with the most colorful pattern.");
@@ -81,14 +115,45 @@ app.post('/socks', async (req, res) => {
         res.status(500).send("Hmmm, something smells... No socks for you! ☹");
     }
 });
-app.delete('/socks/:id', async (req, res) => {
+
+app.post('/socks', async (req, res) => {
     try {
-        const { id } = req.params;
-        console.log('Deleting sock with ID:', id);
-        res.status(200).send('Sock deleted successfully');
+        // TODO: Add code that adds a sock when a new sock is posted using the
+        // Add Sock form.
+        const sock = req.body;
+        const client = await MongoClient.connect(url);
+        const db = client.db(dbName);
+        const collection = db.collection(collectionName);
+        const result = await collection.insertOne(sock);
+        res.status(201).json({
+            message: 'Sock added successfully',
+            sockId: result.insertedId
+        });
     } catch (err) {
         console.error('Error:', err);
-        res.status(500).send('Hmm, something doesn\'t smell right... Error deleting sock');
+        res.status(500).send('Hmm, something doesn\'t smell right... Error adding sock');
+    }
+});
+// app.delete('/socks/:id', async (req, res) => {
+//     try {
+//         const { id } = req.params;
+//         console.log('Deleting sock with ID:', id);
+//         res.status(200).send('Sock deleted successfully');
+//     } catch (err) {
+//         console.error('Error:', err);
+//         res.status(500).send('Hmm, something doesn\'t smell right... Error deleting sock');
+//     }
+// });
+app.delete("/socks/:id", async (req, res) => {
+    try {
+        const { id } = req.params;
+        console.log("Deleting sock with ID:", id);
+        res.status(200).send("Sock deleted successfully");
+    } catch (err) {
+        console.error("Error:", err);
+        res
+            .status(500)
+            .send("Hmm, something doesn't smell right... Error deleting sock");
     }
 });
 app.put('/user/:id', async (req, res) => {
